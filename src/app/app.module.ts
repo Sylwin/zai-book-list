51ca840c70b342444da6f74c67ed7af31e15d304
Sylwia Olender <sylenit@gmail.com>
2019-11-24 10:30:13 +0100

    update order pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
 
-import { Ng2OrderModule } from 'ng2-order-pipe';
+import { OrderModule } from 'ngx-order-pipe';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { FilterPipe } from './filter.pipe';
@@ -22,7 +22,7 @@ import { DeleteBookModalComponent } from './delete-book-modal/delete-book-modal.
   ],
   imports: [
     BrowserModule,
-    Ng2OrderModule,
+    OrderModule,
     NgbModule,
     NgxPaginationModule,
     FormsModule
