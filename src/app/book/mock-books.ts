a7922c3e0d0b3422d56cf5ea9e06be6fa2037266
Sylwia Olender <sylenit@gmail.com>
2019-11-24 16:46:32 +0100

    change default books

--- src/app/book/mock-books.ts
+++ src/app/book/mock-books.ts
@@ -3,52 +3,52 @@ import {Book} from './book';
 export const BOOKS: Book[] = [
   {
     "id": 1,
-    "title": "Book Number 1",
+    "title": "Krzyżacy",
     "price": 15,
     "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/2010-kodiak-bear-1.jpg/1200px-2010-kodiak-bear-1.jpg",
     "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.",
-    "author": "Mickiewicz Adam",
-    "city": "Warszawa",
-    "year": 2015
+    "author": "Sienkiewicz Henryk",
+    "city": "Katowice",
+    "year": 1900
   },
   {
     "id": 2,
-    "title": "Hello Book Number 3",
+    "title": "Balladyna",
     "price": 20,
     "image": "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     "description": "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
     "author": "Słowacki Juliusz",
-    "city": "Wrocław",
-    "year": 2007
+    "city": "Rzeszów",
+    "year": 1839
   },
   {
     "id": 3,
-    "title": "Yeti reti",
+    "title": "Pan Tadeusz",
     "price": 64,
     "image": "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313__340.jpg",
     "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.",
     "author": "Mickiewicz Adam",
     "city": "Warszawa",
-    "year": 2015
+    "year": 1834
   },
   {
     "id": 4,
-    "title": "Żurawina",
+    "title": "Nad Niemnem",
     "price": 43,
     "image": "https://images.pexels.com/photos/1996333/pexels-photo-1996333.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     "description": "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
-    "author": "Słowacki Juliusz",
-    "city": "Wrocław",
-    "year": 1994
+    "author": "Orzeszkowa Eliza",
+    "city": "Warszawa",
+    "year": 1888
   },
   {
     "id": 5,
-    "title": "Tralala",
+    "title": "Akademia Pana Kleksa",
     "price": 90,
     "image": "https://images.unsplash.com/photo-1516632664305-eda5d6a5bb99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
     "description": "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
     "author": "Brzechwa Jan",
     "city": "Kraków",
-    "year": 2007
+    "year": 1946
   }
 ];
